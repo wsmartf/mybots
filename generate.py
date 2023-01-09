414445a3c6f2912a27371623f35d5d00ad75e33a
wsmartf <wjsmart12@gmail.com>
2023-01-09 17:40:39 -0600

    Change link position

--- generate.py
+++ generate.py
@@ -4,6 +4,8 @@ length = 1
 width = 2
 height = 3
 
+x, y, z = 0, 0, 1.5
+
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
 pyrosim.End()
