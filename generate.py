1e9237aa7c75335c8a5f575fac3f41f939fa1fa6
wsmartf <wjsmart12@gmail.com>
2023-01-09 17:47:52 -0600

    Diagonally stacked boxes

--- generate.py
+++ generate.py
@@ -1,11 +1,13 @@
 import pyrosim.pyrosim as pyrosim
 
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
-x, y, z = 0, 0, 1.5
+x1, y1, z1 = 0, 0, 0.5
+x2, y2, z2 = 1, 0, 1
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Send_Cube(name="Box", pos=[x1,y1,z1] , size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length, width, height])
 pyrosim.End()
