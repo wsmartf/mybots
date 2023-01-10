b269b1d1a77f5cfdd04fcb6f9f387a4dd86442da
wsmartf <wjsmart12@gmail.com>
2023-01-09 18:16:03 -0600

    D complete, 25 towers

--- generate.py
+++ generate.py
@@ -4,10 +4,20 @@ length = 1
 width = 1
 height = 1
 
-x1, y1, z1 = 0, 0, 0.5
-x2, y2, z2 = 1, 0, 1
+x, y, z = 0, 0, 0.5
 
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x1,y1,z1] , size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length, width, height])
+
+for x in range(5):
+    x1 = x
+    for y in range(5):
+        y1 = y
+        for i in range(10):
+            z1 = z+i
+            new_l = pow(length*0.9, i)
+            new_w = pow(width*0.9, i)
+            new_h = pow(height*0.9, i)
+            new_name = "Box",x,y,i
+            pyrosim.Send_Cube(name="Box", pos=[x1,y1,z1] , size=[new_l, new_w, new_h])
+
 pyrosim.End()
